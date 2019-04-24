01c5be5e1af0ab7076610acdcc08c60923004640
Jefferson Chou <cheukyinchou@gmail.com>
2019-04-24 17:35:24 +0800

    Update fileio.cpp

--- fileio.cpp
+++ fileio.cpp
@@ -1,14 +1,40 @@
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <locale>
+#include <sstream>
 using namespace std;
+
+int breakdown(string &str,string &name) {
+  locale loc;
+  bool detectdigit=true;
+  int age;
+  for (int i=0;i<str.length();i++) {
+    if (detectdigit==true) {
+      if (isdigit(str[i],loc)){
+        name=str.substr(0,i-1);
+        string sub=str.substr(i,str.length()-i);
+        stringstream(sub) >> age;
+        detectdigit=false;
+      }
+    } else {
+      if (isalpha(str[i],loc)) {
+        string remain=str.substr(i,str.length()-i);
+        str=remain;
+      }
+    }
+  }
+  return age;
+}
 
 struct Employee {
   int ID;
+  string all_info;
   string name;
   int age;
   string role;
   int salary;
-}
+};
 int main() {
   ifstream fin("employees.txt");
   if (fin.fail()) {
@@ -16,12 +42,20 @@ int main() {
     exit(1);
   }
   int temp;
-  Employee *list=new Employee [];
-  count=0;
+  int count=1;
+  Employee *list=new Employee [count];
   while(fin>>temp) {
-    Employee[count].ID=temp;
-    
+    list[count-1].ID=temp;
+    getline(fin,list[count-1].all_info);
     count++;
+
   }
+  fin.close();
+
+  for (int i=0;i<count;++i) {
+    list[i].age=breakdown(list[i].all_info,list[i].name);
+    list[i].salary=breakdown(list[i].all_info,list[i].role);
+  }
+  delete [] list;
   return 0;
 }
