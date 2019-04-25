532083685fb24bebdfdc26f21f7f639accd983e0
Jefferson Chou <cheukyinchou@gmail.com>
2019-04-25 10:10:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,45 +18,49 @@ int main()
   cout<<setfill('-');
   cout<<setw(17)<<' '<<"Staff Management System"<<setw(17)<<' '<<endl;
   cout<<"Menu"<<endl;
-  cout<<"1: Add a new employee"<<endl;
-  cout<<"2: Search for an employee"<<endl;
-  cout<<"3. Fire an employee"<<endl;
-  cout<<"4. Edit the information of an existing employee"<<endl;
-  cout<<"5. Search for employees according to the salary level"<<endl;
-  cout<<"6. Add a defined attribute"<<endl;
-  cout<<"7. Show the current project an employee is engaging in"<<endl;
-  cout<<"8. Add remark to an employee"<<endl;
-  cout<<"9. Exit the system"<<endl;
+  cout<<"1: List all employees"<<endl;
+  cout<<"2: Add a new employee"<<endl;
+  cout<<"3: Search for an employee"<<endl;
+  cout<<"4. Fire an employee"<<endl;
+  cout<<"5. Edit the information of an existing employee"<<endl;
+  cout<<"6. Search for employees according to the salary level"<<endl;
+  cout<<"7. Add a defined attribute"<<endl;
+  cout<<"8. Show the current project an employee is engaging in"<<endl;
+  cout<<"9. Add remark to an employee"<<endl;
+  cout<<"0. Exit the system"<<endl;
   cout<<setw(57)<<' '<<endl;
   cout<<"Please enter your choice :";
   cin>>choice;
   switch(choice)
   {
     case 1:
+     //ListEmployee();
+     break; 
+    case 2:
     //AddEmployee();
     break;
-    case 2:
+    case 3:
     //SearchEmployee();
     break;
-    case 3:
+    case 4:
     //FireEmployee();
     break;
-    case 4:
+    case 5:
     //EditEmployee();
     break;
-    case 5:
+    case 6:
     //SearchSalary();
     break;
-    case 6:
+    case 7:
     //AddAttribute();
     break;
-    case 7:
+    case 8:
     //ShowProject();
     break;
-    case 8:
+    case 9:
     //AddRemark();
     break;
-    case 9:
+    case 0:
     cout<<"Thanks for using."<<endl;
     exit(1);
     default:
