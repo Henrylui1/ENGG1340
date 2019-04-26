d465571e4b88fa27f0994ce48b3d39a80ed798a4
Jefferson Chou <cheukyinchou@gmail.com>
2019-04-26 19:10:54 +0800

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,21 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int breakdown(string,string);
+int ListEmployee();
+void AddEmployee();
+void SearchNamePrintString(Employee *,string,int);
+void SearchEmployee();
+void FireEmployee();
+void EditEmployee();
+void SearchSalary();
+void AddAttribute(Employee *,int);
+void ReadAttribute();
+void EditAttribute(Employee *,int);
+int ViewAttribute(Employee *,int);
+void Attribute();
+void Bubblesortstring(string[],int);
+void Bubblesortnumber(int[],int);
+void Sort();
+
+#endif
